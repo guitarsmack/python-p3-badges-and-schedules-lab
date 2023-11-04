62b9bad2fe2e5302e3c14ce3f7354d722acc1159
Michael Alan Stowe <mastowe94@gmail.com>
2023-11-04 16:25:21 -0500

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for n in names:
+        badges.append(f'Hello, my name is {n}.')
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    room = 1
+    for n in names:
+        rooms.append(f"Hello, {n}! You'll be assigned to room {room}!")
+        room += 1
+    return rooms
 
 def printer(names):
-    return None
+    for n in batch_badge_creator(names):
+        print(n)
+    for n in assign_rooms(names):
+        print(n)
